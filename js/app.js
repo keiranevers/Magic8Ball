ca1d88951140c2e6d2a3b1e1d9cdb60298803773
Keira Nevers <keira.nevers@gmail.com>
2018-02-03 13:47:53 -0500

    push changes

--- js/app.js
+++ js/app.js
@@ -2,35 +2,33 @@ $(document).ready(function() {
 
     $('select').material_select();
     
-    $('.carousel').carousel();
+    $('.materialboxed').materialbox();
 
     //$('.carousel.carousel-slider').carousel({fullWidth: true});
 
     $("#submit").on("click", function(event) {
     	event.preventDefault();
-    	console.log("test");
-    	var queryURL = "https://api.giphy.com/v1/gifs/search?q='box+of+kittens'&api_key=BLZeUFUso54cVFZ9RU3N0aCuaP8WB3Jw&limit=5";
+    	
+    	var queryURL = "http://api.giphy.com/v1/gifs/random?&api_key=BLZeUFUso54cVFZ9RU3N0aCuaP8WB3Jw&tag=kittens";
+	    
 	    $.ajax({
 	        url: queryURL,
 	        method: "GET"
 	      }).then(function(response) {
 
-	      	var results = response.data
-	      	console.log(results);	      	
+	      	var results = response.data;
 	      	
-	      	var resultDiv = $("<div>").addClass("carousel");
-	      	$("#resultTable").append(resultDiv);
 
-	      	for (var i = 0; i < results.length; i++) {
-	      		var image = results[i].images.fixed_height.url;
-	      		var rating = results[i].rating;
-	      		console.log(image[i]);
-	      		console.log(rating[i]);
+	      	var image = results.image_original_url;
 
-	      		resultDiv.append($("<a class='carousel-item' href='#one!'><img src='" + image[i] + "'><" + rating[i] + "</a>"));
-	      		
-	      		
-	      		}
+	      	var title = results.title;
+
+
+	      	//<img class="materialboxed" data-caption="Get me a Box Of Kittens! STAT!" width="250" src="images/kittens1.jpg">
+
+	      	
+	      	$("#results").append("<img class='materialboxed' data-caption='" + title + "' src='" + image + "'>");
+	      	
 	      	//	
 
 	    });
