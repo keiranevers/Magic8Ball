f6402db81908aecaa40362ad1ee4ccd92ae9d5ea
Keira Nevers <keira.nevers@gmail.com>
2018-02-05 20:07:00 -0500

    updates to index and app

--- js/app.js
+++ js/app.js
@@ -6,32 +6,66 @@ $(document).ready(function() {
 
     //$('.carousel.carousel-slider').carousel({fullWidth: true});
 
-    $("#submit").on("click", function(event) {
+    $("#submit").on("click", function() {
     	event.preventDefault();
+
+    //onclick get random function to run, giphy, nytimes or omdb
+
+    var functionArray = [giphy, omdb, nytimes];
+    
+    var randomSelect = Math.floor(Math.random() * 3);
+    console.log(randomSelect);
+
+    var functionCall = functionArray[randomSelect]().then(function(result) {
+    });
+    
+    		$("#nameInput").val("");
+		    $("#monthInput").val("Month");
+		    $("#dayInput").val("Day");
+    //based on random return, call function (giphy, nytimes or omdb) with if statement, else last should be giphy if no results are returned from omdb or nytimes
+
+    console.log(functionCall);
     	
-    	var queryURL = "http://api.giphy.com/v1/gifs/random?&api_key=BLZeUFUso54cVFZ9RU3N0aCuaP8WB3Jw&tag=kittens";
-	    
-	    $.ajax({
-	        url: queryURL,
-	        method: "GET"
-	      }).then(function(response) {
-
-	      	var results = response.data;
-	      	
-
-	      	var image = results.image_original_url;
-
-	      	var title = results.title;
-
-
-	      	//<img class="materialboxed" data-caption="Get me a Box Of Kittens! STAT!" width="250" src="images/kittens1.jpg">
-
-	      	
-	      	$("#results").append("<img class='materialboxed' data-caption='" + title + "' src='" + image + "'>");
-	      	
-	      	//	
-
-	    });
-
+    
   	});
-  });  
+
+	var giphy = function() {
+
+	  	var queryURL = "http://api.giphy.com/v1/gifs/random?&api_key=BLZeUFUso54cVFZ9RU3N0aCuaP8WB3Jw&tag=kittens";
+		    
+		    return $.ajax({
+		        url: queryURL,
+		        method: "GET"
+		    }).then(function(response) {
+
+		      	var results = response.data;
+		      	
+
+		      	var image = results.image_original_url;
+
+		      	var title = results.title;
+
+
+		      	//<img class="materialboxed" data-caption="Get me a Box Of Kittens! STAT!" width="250" src="images/kittens1.jpg">
+
+		      	
+		      	$("#results").append("<img class='materialboxed' data-caption='" + title + "' src='" + image + "'>");
+		      	
+		      	//clear form fields
+
+				});
+
+		};
+
+	var omdb = function() {
+
+
+	};
+
+var nytimes = function() {
+
+
+};
+
+//end the document ready function
+});  
